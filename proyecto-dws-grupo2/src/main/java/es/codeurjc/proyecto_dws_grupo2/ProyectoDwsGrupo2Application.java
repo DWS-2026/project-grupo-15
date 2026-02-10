4e0a99898bd2cd6c219161e58b86c27eca5d3eba
D_Puga <d.puga.2024@alumnos.urjc.es>
2026-02-10 09:48:20 +0100

    Creacion proyecto spring

---
+++ proyecto-dws-grupo2/src/main/java/es/codeurjc/proyecto_dws_grupo2/ProyectoDwsGrupo2Application.java
@@ -0,0 +1,13 @@
+package es.codeurjc.proyecto_dws_grupo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDwsGrupo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDwsGrupo2Application.class, args);
+	}
+
+}
---
+++ proyecto-dws-grupo2/src/test/java/es/codeurjc/proyecto_dws_grupo2/ProyectoDwsGrupo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.codeurjc.proyecto_dws_grupo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDwsGrupo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
